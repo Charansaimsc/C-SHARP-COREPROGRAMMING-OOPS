90fa97115527a18aede2ad6a28a881ae39d1f6a9
Charansaimsc <charanmsc7969@gmail.com>
2025-12-29 23:09:59 +0530

    [charan] Add.Bank Acoount Porblem using Different Keywords

---
+++ Keywords/BankAccount.cs
@@ -0,0 +1,54 @@
+﻿/*Create a BankAccount class with the following features:
+● static:
+o A static variable bankName shared across all accounts.
+o A static method GetTotalAccounts() to display the total number of accounts.
+● this:
+o Use this to resolve ambiguity in the constructor when initializing
+AccountHolderName and AccountNumber.
+
+● readonly:
+o Use a readonly variable AccountNumber to ensure it cannot be changed once
+assigned.
+● is operator:
+o Check if an account object is an instance of the BankAccount class before
+displaying its details.*/
+
+
+
+using System;
+
+class BankAccount
+{
+ 
+    public static string BankName = "State Bank of India";
+    private static int totalAccounts = 0;
+
+    public static void GetTotalAccounts()
+    {
+        Console.WriteLine("Total Accounts Created: " + totalAccounts);
+    }
+
+   
+    public string AccountHolderName;
+    public readonly int AccountNumber;  
+    public double Balance;
+
+   
+    public BankAccount(string accountHolderName, int accountNumber, double balance)
+    {
+        this.AccountHolderName = accountHolderName;
+        this.AccountNumber = accountNumber; 
+        this.Balance = balance;
+
+        totalAccounts++; 
+    }
+
+    public void DisplayDetails()
+    {
+        Console.WriteLine("Bank Name       : " + BankName);
+        Console.WriteLine("Account Holder  : " + AccountHolderName);
+        Console.WriteLine("Account Number  : " + AccountNumber);
+        Console.WriteLine("Balance         : " + Balance);
+        Console.WriteLine("--------------------------------");
+    }
+}
--- Keywords/Program.cs
+++ Keywords/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace keywords
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount acc1 = new BankAccount("Charan", 101, 50000);
+            BankAccount acc2 = new BankAccount("Sai", 102, 75000);
+
+
+            if (acc1 is BankAccount)
+            {
+                acc1.DisplayDetails();
+            }
+
+            if (acc2 is BankAccount)
+            {
+                acc2.DisplayDetails();
+            }
+
+            BankAccount.GetTotalAccounts();
+
+        }  
+    }
+}
