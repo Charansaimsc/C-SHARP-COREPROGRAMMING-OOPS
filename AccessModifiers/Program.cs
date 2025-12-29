87c1b9bc9253e2cfda5399d05b2e75a316feb0f3
Charansaimsc <charanmsc7969@gmail.com>
2025-12-29 14:15:44 +0530

    [charan] Add.University Management system solution

--- AccessModifiers/Program.cs
+++ AccessModifiers/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace AcessModifiers
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Base class object
+            Student s1 = new Student(101, "Charan", 8.5);
+            s1.DisplayStudent();
+
+            Console.WriteLine();
+
+            // Modify CGPA using public method
+            s1.SetCGPA(9.1);
+            Console.WriteLine("Updated CGPA: " + s1.GetCGPA());
+
+            Console.WriteLine();
+
+            // Derived class object
+            PostgraduateStudent pg = new PostgraduateStudent(201, "Sai", 8.9, "Artificial Intelligence");
+            pg.DisplayPostgraduateDetails();
+        }
+    }
+}
--- AccessModifiers/UniversityManagementSystem.cs
+++ AccessModifiers/UniversityManagementSystem.cs
@@ -1,8 +1,85 @@
-﻿using System;
+﻿/*Create a Student class with:
+o rollNumber (public)
+o name (protected)
+o CGPA (private)
 
-public class Class1
+● Implement methods to:
+o Access and modify CGPA using public methods.
+o Create a subclass PostgraduateStudent to demonstrate the use of protected
+members.*/
+
+
+
+
+using System;
+
+// Base Class
+class Student
 {
-	public Class1()
-	{
-	}
+    // Public variable
+    public int rollNumber;
+
+    // Protected variable
+    protected string name;
+
+    // Private variable
+    private double CGPA;
+
+    // Constructor
+    public Student(int rollNumber, string name, double cgpa)
+    {
+        this.rollNumber = rollNumber;
+        this.name = name;
+        this.CGPA = cgpa;
+    }
+
+    // Public method to get CGPA
+    public double GetCGPA()
+    {
+        return CGPA;
+    }
+
+    // Public method to set CGPA
+    public void SetCGPA(double cgpa)
+    {
+        if (cgpa >= 0 && cgpa <= 10)
+        {
+            CGPA = cgpa;
+        }
+        else
+        {
+            Console.WriteLine("Invalid CGPA value.");
+        }
+    }
+
+    // Method to display basic student details
+    public void DisplayStudent()
+    {
+        Console.WriteLine("Roll Number : " + rollNumber);
+        Console.WriteLine("Name        : " + name);
+        Console.WriteLine("CGPA        : " + CGPA);
+    }
 }
+
+// Derived Class
+class PostgraduateStudent : Student
+{
+    public string specialization;
+
+    // Constructor
+    public PostgraduateStudent(int rollNumber, string name, double cgpa, string specialization)
+        : base(rollNumber, name, cgpa)
+    {
+        this.specialization = specialization;
+    }
+
+    // Method demonstrating access to protected member
+    public void DisplayPostgraduateDetails()
+    {
+        Console.WriteLine("Roll Number     : " + rollNumber);  // public
+        Console.WriteLine("Name            : " + name);        // protected
+        Console.WriteLine("Specialization  : " + specialization);
+        Console.WriteLine("CGPA            : " + GetCGPA());   // accessed via public method
+    }
+}
+
