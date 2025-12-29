b884a013aa69625512183665d4fad9e5dc583522
Charansaimsc <charanmsc7969@gmail.com>
2025-12-29 11:25:53 +0530

    [charan] Add.ProdctInventary pROBLEM SOLUTION

---
+++ Instances/ProductInventary.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Product
+{
+	string productName;
+	int price;
+	static int totalStudents;
+	public Product(string productName,int price)
+	{
+		this.productName = productName;
+		this.price = price;
+		totalStudents++;
+    }
+	public void DisplayProductDetails()
+	{
+		Console.WriteLine(productName + "   " + price);
+	}
+	public static void DisplayTotalStudents()
+	{
+		Console.WriteLine(totalStudents);
+	}
+	
+}
--- Instances/Program.cs
+++ Instances/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace instances
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p1 = new Product("salt", 1000);
+
+            Product p2 = new Product("sugar", 12200);
+
+            p1.DisplayProductDetails();
+            Product.DisplayTotalStudents();
+        }
+    }
+}
