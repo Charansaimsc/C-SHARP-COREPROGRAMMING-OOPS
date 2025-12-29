3c92ae203e022b5a82b01d5cbf05a1811bd84dcc
Charansaimsc <charanmsc7969@gmail.com>
2025-12-29 10:32:29 +0530

    [charan] Add.Book class Solution

---
+++ Constructors/Level_1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Level_1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Book obj = new Book();
+            obj.Display();
+            Book obj2 = new Book("alice", "asdf", 199);
+            obj2.Display();
+        }
+    }
+}
