c1117f26b0cc68f84711e762b07c3c2beb7a5756
Charansaimsc <charanmsc7969@gmail.com>
2025-12-29 12:13:12 +0530

    [charan] Add.Vehicle info Solution

--- Instances/Program.cs
+++ Instances/Program.cs
@@ -14,21 +14,27 @@ namespace instances
             //Product.DisplayTotalStudents();
 
 
-            Course course1 = new Course("C# Programming", 3, 15000);
-            Course course2 = new Course("Machine Learning", 6, 35000);
+            //Course course1 = new Course("C# Programming", 3, 15000);
+            //Course course2 = new Course("Machine Learning", 6, 35000);
 
-            // Display course details
-            course1.DisplayCourseDetails();
-            course2.DisplayCourseDetails();
+            //// Display course details
+            //course1.DisplayCourseDetails();
+            //course2.DisplayCourseDetails();
 
-            // Update institute name for all courses
-            Course.UpdateInstituteName("Tech Minds Academy");
+            //// Update institute name for all courses
+            //Course.UpdateInstituteName("Tech Minds Academy");
 
-            Console.WriteLine("After Updating Institute Name:\n");
+            //Console.WriteLine("After Updating Institute Name:\n");
 
-            // Display details again
-            course1.DisplayCourseDetails();
-            course2.DisplayCourseDetails();
+            //// Display details again
+            //course1.DisplayCourseDetails();
+            //course2.DisplayCourseDetails();
+
+
+            Vehicle v1 = new Vehicle("sai", "car");
+            Vehicle v2 = new Vehicle("charan", "bike");
+            v1.DisplayVehicleInfo();
+            Vehicle.UpdateFee(200);
         }
     }
 }
---
+++ Instances/VehicleRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Vehicle
+{
+	string ownerName;
+	string vehicleType;
+
+	static int registrationFee= 100;
+
+	public Vehicle(string ownerName,string vehicleType)
+	{
+		this.ownerName = ownerName;
+		this.vehicleType = vehicleType;
+	}
+	public void DisplayVehicleInfo()
+	{
+		Console.WriteLine("vehicle owner :"+ownerName + "\n Typr   : " + vehicleType);
+	}
+	public static void UpdateFee(int newFee)
+	{
+		registrationFee = newFee;
+
+		Console.WriteLine(newFee);
+	}
+}
