36b98648bbc10024e97f9d7c0f52ad2eb9934a1a
Charansaimsc <charanmsc7969@gmail.com>
2025-12-29 11:54:42 +0530

    [charan] Add.Online Couse Management Solution

---
+++ Instances/OnlineCouseManagement.cs
@@ -0,0 +1,47 @@
+﻿/*Online Course Management
+● Design a Course class with:
+o Instance Variables: courseName, duration, fee.
+o Class Variable: instituteName (common for all courses).
+● Implement the following methods:
+o An instance method DisplayCourseDetails() to display course details.
+o A class method UpdateInstituteName() to modify the institute name for all
+courses.*/
+
+
+using System;
+
+class Course
+{
+    // Instance Variables
+    string courseName;
+    int duration;     // in months
+    double fee;
+
+    // Class Variable (shared by all courses)
+    static string instituteName = "ABC Institute";
+
+    // Parameterized Constructor
+    public Course(string courseName, int duration, double fee)
+    {
+        this.courseName = courseName;
+        this.duration = duration;
+        this.fee = fee;
+    }
+
+    // Instance Method
+    public void DisplayCourseDetails()
+    {
+        Console.WriteLine("Institute Name : " + instituteName);
+        Console.WriteLine("Course Name    : " + courseName);
+        Console.WriteLine("Duration       : " + duration + " months");
+        Console.WriteLine("Fee            : " + fee);
+        Console.WriteLine();
+    }
+
+    // Class Method (Static Method)
+    public static void UpdateInstituteName(string newInstituteName)
+    {
+        instituteName = newInstituteName;
+    }
+}
+
--- Instances/Program.cs
+++ Instances/Program.cs
@@ -6,12 +6,29 @@ namespace instances
     {
         static void Main(string[] args)
         {
-            Product p1 = new Product("salt", 1000);
+            //Product p1 = new Product("salt", 1000);
 
-            Product p2 = new Product("sugar", 12200);
+            //Product p2 = new Product("sugar", 12200);
 
-            p1.DisplayProductDetails();
-            Product.DisplayTotalStudents();
+            //p1.DisplayProductDetails();
+            //Product.DisplayTotalStudents();
+
+
+            Course course1 = new Course("C# Programming", 3, 15000);
+            Course course2 = new Course("Machine Learning", 6, 35000);
+
+            // Display course details
+            course1.DisplayCourseDetails();
+            course2.DisplayCourseDetails();
+
+            // Update institute name for all courses
+            Course.UpdateInstituteName("Tech Minds Academy");
+
+            Console.WriteLine("After Updating Institute Name:\n");
+
+            // Display details again
+            course1.DisplayCourseDetails();
+            course2.DisplayCourseDetails();
         }
     }
 }
