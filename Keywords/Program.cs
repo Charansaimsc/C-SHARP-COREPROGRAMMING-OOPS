88cccf7c0a988815282c09b40dd09437f50053ec
Charansaimsc <charanmsc7969@gmail.com>
2025-12-30 10:40:07 +0530

    [charan] Add.Vehicle regidtration info solution

--- Keywords/Program.cs
+++ Keywords/Program.cs
@@ -5,21 +5,32 @@ namespace keywords
     {
         static void Main(string[] args)
         {
-            BankAccount acc1 = new BankAccount("Charan", 101, 50000);
-            BankAccount acc2 = new BankAccount("Sai", 102, 75000);
+            //BankAccount acc1 = new BankAccount("Charan", 101, 50000);
+            //BankAccount acc2 = new BankAccount("Sai", 102, 75000);
 
 
-            if (acc1 is BankAccount)
+            //if (acc1 is BankAccount)
+            //{
+            //    acc1.DisplayDetails();
+            //}
+
+            //if (acc2 is BankAccount)
+            //{
+            //    acc2.DisplayDetails();
+            //}
+
+            //BankAccount.GetTotalAccounts();
+
+
+            Vehicle v1 = new Vehicle("Charan", "2-wheeler", 7129);
+            Vehicle v2 = new Vehicle("sai", "4-wheeler", 7129);
+
+            if(v1 is Vehicle)
             {
-                acc1.DisplayDetails();
+                Vehicle.UpdateRegisytrationFee(100);
+                v1.Display();
+             
             }
-
-            if (acc2 is BankAccount)
-            {
-                acc2.DisplayDetails();
-            }
-
-            BankAccount.GetTotalAccounts();
 
         }  
     }
---
+++ Keywords/VehicleRegistrationSystem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Vehicle
+{
+	public static int  RegistrationFee;
+
+	public static void  UpdateRegisytrationFee(int newRegistrationFee)
+	{
+		RegistrationFee = newRegistrationFee;
+	}
+
+	public string OwnerName;
+	public string VehicleType;
+		public readonly int VehicleNumber;
+	public Vehicle(string OwnerName,string VehicleType,int VehicleNumber)
+	{
+		this.OwnerName = OwnerName;
+		this.VehicleNumber = VehicleNumber;	
+		this.VehicleType = VehicleType;
+	}
+	public void Display()
+	{
+		Console.WriteLine("VehicleNumber : " + VehicleNumber + "]\nOwnerName : "+OwnerName + "\nvEHICLEtYPE : " + VehicleType + "\n" + RegistrationFee);
+	}
+
+}
