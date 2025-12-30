f936f245907f807818240327a1e08d4b8d526951
Charansaimsc <charanmsc7969@gmail.com>
2025-12-30 11:02:43 +0530

    [charan] Add.uNIVERSITYsTUDENTmANAGEMENTsYSTEM Solution

--- Keywords/Program.cs
+++ Keywords/Program.cs
@@ -22,16 +22,25 @@ namespace keywords
             //BankAccount.GetTotalAccounts();
 
 
-            Vehicle v1 = new Vehicle("Charan", "2-wheeler", 7129);
-            Vehicle v2 = new Vehicle("sai", "4-wheeler", 7129);
+            //Vehicle v1 = new Vehicle("Charan", "2-wheeler", 7129);
+            //Vehicle v2 = new Vehicle("sai", "4-wheeler", 7129);
 
-            if(v1 is Vehicle)
+            //if(v1 is Vehicle)
+            //{
+            //    Vehicle.UpdateRegisytrationFee(100);
+            //    v1.Display();
+
+            //}
+
+
+
+            Student s1 = new Student("sai", 10, 'c');
+            Student s2 = new Student("sai", 10, 'c');
+            if(s1 is Student)
             {
-                Vehicle.UpdateRegisytrationFee(100);
-                v1.Display();
-             
+                s1.Display();
+                Student.DisplayTotalStudents();
             }
-
-        }  
+        }
     }
 }
---
+++ Keywords/UniversityStudentManagement.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Student
+{
+	public static string UniversityName = "GLA UNIV";
+	public string StudentName { get; set; }
+	public  static int totalStudents = 0;
+	public static void DisplayTotalStudents()
+	{
+		Console.WriteLine("Total Students: " + totalStudents);
+	}
+
+
+    //public string Name;
+    public readonly int roll;
+	public char Grade;
+	public Student(string Name, int roll, char Grade)
+	{
+		this.StudentName = Name;
+		this.roll = roll;
+		this.Grade = Grade;
+		totalStudents++;
+	}
+	public void Display() {
+		Console.WriteLine("StudentName : " + StudentName + "\nUniversityName : " + UniversityName + "\nroll : " + roll + "\nGrade ; " + Grade + "        \n" + totalStudents);
+	}
+}
