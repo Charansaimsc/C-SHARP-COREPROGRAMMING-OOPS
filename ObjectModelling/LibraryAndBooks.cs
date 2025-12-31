5b130fe89748468475e6d64cfc3840c819d4b957
Charansaimsc <charanmsc7969@gmail.com>
2025-12-31 14:59:15 +0530

    [charan] Add.LibraryAndBooks Solution

---
+++ ObjectModelling/LibraryAndBooks.cs
@@ -0,0 +1,48 @@
+﻿/*Description: Create a Library class that contains multiple Book objects. Model the
+relationship such that a library can have many books, but a book can exist independently
+(outside of a specific library).
+Tasks:
+● Define a Library class with a List<Book> collection.
+● Define a Book class with attributes such as Title and Author.
+● Demonstrate the aggregation relationship by creating books and adding them to
+different libraries.
+Goal: Understand aggregation by modeling a real-world relationship where the Library
+aggregates Book objects.*/
+
+
+using System;
+
+public class Book
+{
+	public string Title {  get; set; }
+public string Author {  get; set; }
+
+	public Book(string Title,string Author)
+	{
+	this.Title = Title;
+	this.Author = Author;
+	}
+
+}
+public class Library
+{
+	public string LibraryName { get; set; }
+	public List<Book> Books;
+	public Library(string LibraryName)
+	{
+		this.LibraryName = LibraryName;
+		Books = new List<Book>();
+	}
+	public void AddBook(Book book)
+	{
+		Books.Add(book);
+	}
+	public void Display()
+	{
+		for (int i = 0; i < Books.Count; i++)
+		{
+			Book book = Books[i];
+			Console.WriteLine(book.Title + book.Author);
+		}
+	}
+}
--- ObjectModelling/Program.cs
+++ ObjectModelling/Program.cs
@@ -5,13 +5,32 @@ namespace Objectss
     {
         static void Main(string[] args)
         {
-            Bank bank =new Bank("sbi");
-            Customer c1 = new Customer("sai");
-            Customer c2 = new Customer("charan");
-            bank.OpenAccount(c1, 101, 100);
-            bank.OpenAccount(c2, 102, 1000);
-            c1.ViewBalance();
-            c1.ViewBalance();
+            //
+            // Bank bank =new Bank("sbi");
+            //    Customer c1 = new Customer("sai");
+            //    Customer c2 = new Customer("charan");
+            //    bank.OpenAccount(c1, 101, 100);
+            //    bank.OpenAccount(c2, 102, 1000);
+            //    c1.ViewBalance();
+            //    c1.ViewBalance();
+            //
+
+
+
+            Book b1 = new Book("c#", "sai");
+            Book b2 = new Book("csharp", "charan");
+            Book b3 = new Book("programing", "sais");
+            Library lib1 = new Library("Central Library");
+            Library lib2 = new Library("Community Library");
+
+            lib1.AddBook(b1);
+            lib1.AddBook(b2);
+            lib2.AddBook(b3);
+
+
+            lib1.Display();
+            lib2.Display();
+
         }
     }
 }
