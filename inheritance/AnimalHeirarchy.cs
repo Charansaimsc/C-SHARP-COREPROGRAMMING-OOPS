dab3cfa41e9177a648d6d91886180e055497e416
Charansaimsc <charanmsc7969@gmail.com>
2026-01-02 10:52:46 +0530

    [charan] Update.AnimalHeirarchy Problem

--- inheritance/AnimalHeirarchy.cs
+++ inheritance/AnimalHeirarchy.cs
@@ -3,7 +3,16 @@
 public class Animal
 {
 	public string Name { get; set; }
-	public string Age { get; set; }
+	public int  Age { get; set; }
+	public Animal(string Name,int Age)
+	{
+		this.Name = Name;
+		this.Age = Age;
+	}
+	public void Display()
+	{
+		Console.WriteLine(Name + " " + Age);
+	}
 	public virtual void MakeSound()
 	{
 		Console.WriteLine("Animal makes sound");
@@ -12,22 +21,32 @@ public class Animal
 }
 public class Dog : Animal
 {
+	public Dog(string Name, int age) : base(Name, age) { }
 	public override void MakeSound()
 	{
+		base.Display();
 		Console.WriteLine("Dog Barks");
 	}
 }
 public class Cat : Animal
 {
+
+    public Cat(string Name, int age) : base(Name, age) { }
     public override void MakeSound()
     {
-        Console.WriteLine("cat Meows");
+
+        base.Display();
+        Console.WriteLine("cat Meows"+"\n");
     }
 }
 public class Bird : Animal
 {
+
+    public Bird(string Name, int age) : base(Name, age) { }
     public override void MakeSound()
     {
+
+        base.Display();
         Console.WriteLine("Bird also sounds ");
     }
 }
--- inheritance/Program.cs
+++ inheritance/Program.cs
@@ -5,8 +5,10 @@ namespace inheritance
     {
         public static void Main(string[] args)
         {
-            Animal A = new Dog();
+            Animal A = new Dog("max",2);
             A.MakeSound();
+            Animal B = new Cat("maxi",3);
+            B.MakeSound();
         }
     }
 }
