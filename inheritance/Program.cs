4852b095dfbe2e76d87662afb7dbd668a8bef9f6
Charansaimsc <charanmsc7969@gmail.com>
2026-01-02 11:58:49 +0530

    [charan] Add .Vehicle Tansport System  Problem

--- inheritance/Program.cs
+++ inheritance/Program.cs
@@ -10,10 +10,22 @@ namespace inheritance
             //Animal B = new Cat("maxi",3);
             //B.MakeSound();
 
-            Employee e1 = new Intern("Sai", 122, 100.99, "c#");
-            e1.DisplayDetails();
-            Employee e2 = new Manager(100,"charan", 101, 1000.99);
-            e2 .DisplayDetails();
+            //Employee e1 = new Intern("Sai", 122, 100.99, "c#");
+            //e1.DisplayDetails();
+            //Employee e2 = new Manager(100,"charan", 101, 1000.99);
+            //e2 .DisplayDetails();
+            Vehicle[] vehicles =
+                   {
+            new Car(5, 180, "Petrol"),
+            new Truck(2000, 120, "Diesel"),
+            new Motorcycle(true, 150, "Petrol")
+        };
+
+            foreach (Vehicle v in vehicles)
+            {
+                v.DisplayInfo();
+                Console.WriteLine();
+            }
         }
     }
 }
---
+++ inheritance/VehicleTransportSystem.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+public class Vehicle
+{
+    public int MaxSpeed { get; set; }
+    public string FuelType { get; set; }
+
+    public Vehicle(int maxSpeed, string fuelType)
+    {
+        MaxSpeed = maxSpeed;
+        FuelType = fuelType;
+    }
+
+    public virtual void DisplayInfo()
+    {
+        Console.WriteLine($"MaxSpeed: {MaxSpeed}  FuelType: {FuelType}");
+    }
+}
+
+public class Car : Vehicle
+{
+    public int SeatCapacity { get; set; }
+
+    public Car(int seatCapacity, int maxSpeed, string fuelType)
+        : base(maxSpeed, fuelType)
+    {
+        SeatCapacity = seatCapacity;
+    }
+
+    public override void DisplayInfo()
+    {
+        base.DisplayInfo();
+        Console.WriteLine($"Seat Capacity: {SeatCapacity}");
+    }
+}
+
+public class Truck : Vehicle
+{
+    public int PayloadCapacity { get; set; }
+
+    public Truck(int payloadCapacity, int maxSpeed, string fuelType)
+        : base(maxSpeed, fuelType)
+    {
+        PayloadCapacity = payloadCapacity;
+    }
+
+    public override void DisplayInfo()
+    {
+        base.DisplayInfo();
+        Console.WriteLine($"Payload Capacity: {PayloadCapacity}");
+    }
+}
+
+public class Motorcycle : Vehicle
+{
+    public bool HasSideCar { get; set; }
+
+    public Motorcycle(bool hasSideCar, int maxSpeed, string fuelType)
+        : base(maxSpeed, fuelType)
+    {
+        HasSideCar = hasSideCar;
+    }
+
+    public override void DisplayInfo()
+    {
+        base.DisplayInfo();
+        Console.WriteLine($"Has SideCar: {HasSideCar}");
+    }
+}
+
+
