4a8a23479243cbf4fd96591964c7ef92416cc96f
Charansaimsc <charanmsc7969@gmail.com>
2026-01-02 22:43:34 +0530

    [charan] Add.Branch Initiated

---
+++ oop-pillars/EmployeeManagementSystem.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+public interface IDepartment
+{
+    void AssignDepartment(string departmentName);
+    string GetDepartmentDetails();
+}
+public abstract class Employee : IDepartment
+{
+    protected int employeeId;
+    private double baseSalary;
+    private string department;
+    public int EmployeeId
+    {
+        get { return employeeId; }
+    }
+
+    public string Name { get; set; }
+
+    protected double BaseSalary
+    {
+        get { return baseSalary; }
+        set { baseSalary = value; }
+    }
+
+    public Employee(int employeeId, string name, double baseSalary)
+    {
+        this.employeeId = employeeId;
+        this.Name = name;
+        this.baseSalary = baseSalary;
+    }
+
+    public abstract double CalculateSalary();
+    public void DisplayDetails()
+    {
+        Console.WriteLine( $"ID: {EmployeeId}, Name: {Name}, Salary: {CalculateSalary()}, Department: {department}");
+    }
+    public void AssignDepartment(string departmentName)
+    {
+        department = departmentName;
+    }
+
+    public string GetDepartmentDetails()
+    {
+        return department;
+    }
+}
+public class FullTimeEmployee : Employee
+{
+    private int workingHours;
+    private double hourlyRate;
+
+    public FullTimeEmployee(int employeeId, string name, double hourlyRate, int workingHours)
+        : base(employeeId, name, 0)
+    {
+        this.hourlyRate = hourlyRate;
+        this.workingHours = workingHours;
+    }
+
+    public override double CalculateSalary()
+    {
+        return hourlyRate * workingHours;
+    }
+}
+public class PartTimeEmployee : Employee
+{
+    private double fixedSalary;
+
+    public PartTimeEmployee(int employeeId, string name, double fixedSalary)
+        : base(employeeId, name, fixedSalary)
+    {
+        this.fixedSalary = fixedSalary;
+    }
+
+    public override double CalculateSalary()
+    {
+        return fixedSalary;
+    }
+}
+
--- oop-pillars/Program.cs
+++ oop-pillars/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace ooppillar
+{
+
+    public class Program
+{
+    static void Main(string[] args)
+    {
+
+        List<Employee> employees = new List<Employee>();
+        Employee emp1 = new FullTimeEmployee(101, "Charan", 500, 20);
+        emp1.AssignDepartment("IT");
+        Employee emp2 = new PartTimeEmployee(102, "Sai", 15000);
+        emp2.AssignDepartment("HR");
+        employees.Add(emp1);
+        employees.Add(emp2);
+        foreach (Employee emp in employees)
+        {
+            emp.DisplayDetails();
+        }
+
+    }
+}
+}
