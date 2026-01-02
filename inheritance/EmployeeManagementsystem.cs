45475c4c730702d215dfba4a9aa3f5cd68ed1f23
Charansaimsc <charanmsc7969@gmail.com>
2026-01-02 11:34:12 +0530

    [charan] Update EmployeeManagementSystem Problem

--- inheritance/EmployeeManagementsystem.cs
+++ inheritance/EmployeeManagementsystem.cs
@@ -1,8 +1,66 @@
 ﻿using System;
 
-public class Class1
+public class Employee
 {
-	public Class1()
+	public string Name { get; set; }
+	public int Id { get; set; }
+	public double Salary { get; set; }
+	public Employee(string Name,int Id,double Salary)
 	{
+		this.Name = Name;
+		this.Id = Id;
+		this.Salary = Salary;
 	}
+	public virtual void DisplayDetails()
+	{
+		Console.WriteLine(Name + "    " + Id + "      " + Salary);
+	}
+}
+public class Manager : Employee
+{
+
+    public int TeamSize { get; set; }
+	public Manager(int  TeamSize,string Name,int Id,double Salary ) : base(Name, Id, Salary)
+	{
+		  
+		this.TeamSize = TeamSize;
+	}
+	public override void DisplayDetails()
+	{
+        base.DisplayDetails();
+
+        Console.WriteLine(  "     " + TeamSize);
+	}
+}
+
+public class Developer : Employee
+{
+
+    public string ProgrammingLanguage { get; set; }
+    public Developer( string Name, int Id, double Salary, string ProgrammingLanguage) : base(Name, Id, Salary)
+    {
+
+        this.ProgrammingLanguage = ProgrammingLanguage;
+    }
+    public override void DisplayDetails()
+    {
+        base.DisplayDetails();
+        Console.WriteLine( "     " + ProgrammingLanguage);
+    }
+}
+
+public class Intern : Employee
+{
+
+    public string InternshipDuration { get; set; }
+    public Intern( string Name, int Id, double Salary, string InternshipDuration) : base(Name, Id, Salary)
+    {
+
+        this.InternshipDuration = InternshipDuration;
+    }
+    public override void DisplayDetails()
+    {
+        base.DisplayDetails();
+        Console.WriteLine(  "     " + InternshipDuration);
+    }
 }
--- inheritance/Program.cs
+++ inheritance/Program.cs
@@ -5,10 +5,15 @@ namespace inheritance
     {
         public static void Main(string[] args)
         {
-            Animal A = new Dog("max",2);
-            A.MakeSound();
-            Animal B = new Cat("maxi",3);
-            B.MakeSound();
+            //Animal A = new Dog("max",2);
+            //A.MakeSound();
+            //Animal B = new Cat("maxi",3);
+            //B.MakeSound();
+
+            Employee e1 = new Intern("Sai", 122, 100.99, "c#");
+            e1.DisplayDetails();
+            Employee e2 = new Manager(100,"charan", 101, 1000.99);
+            e2 .DisplayDetails();
         }
     }
 }
