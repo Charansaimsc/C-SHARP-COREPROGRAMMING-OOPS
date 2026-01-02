8e750c69706f14b639a1fa541cc402ec00f6fea0
Charansaimsc <charanmsc7969@gmail.com>
2026-01-02 10:35:12 +0530

    [charan] Add.AnimalHeirarchy Problem

---
+++ inheritance/AnimalHeirarchy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Animal
+{
+	public string Name { get; set; }
+	public string Age { get; set; }
+	public virtual void MakeSound()
+	{
+		Console.WriteLine("Animal makes sound");
+
+	}
+}
+public class Dog : Animal
+{
+	public override void MakeSound()
+	{
+		Console.WriteLine("Dog Barks");
+	}
+}
+public class Cat : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("cat Meows");
+    }
+}
+public class Bird : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Bird also sounds ");
+    }
+}
---
+++ inheritance/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace inheritance
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Animal A = new Dog();
+            A.MakeSound();
+        }
+    }
+}
